2602587ed5f2fc80b199ff309ce8064ce1eb888d
Dusan Radojcic <dusanradojcic00@gmail.com>
2021-06-19 23:03:17 +0200

    Created LoginCOntroller

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField inputEmail;
+
+    @FXML
+    private TextField inputPass;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
+
